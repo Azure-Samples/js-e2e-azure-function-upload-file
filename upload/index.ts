0519eb462a393037a1f8fc86ac3b80b7e7440a9a
Dina Berry <41597107+diberry@users.noreply.github.com>
2022-01-24 14:12:05 -0800

    updates

--- upload/index.ts
+++ upload/index.ts
@@ -7,12 +7,28 @@ Task: accept file, as multipart form request, send file (100MB max on Consumptio
 
 Run the curl command in the same directory as the file:
 
-curl -X POST  -F 'filename=@test-file.txt' 'http://localhost:7071/api/upload?filename=test-file.txt&username=jsmith' --verbose
+curl -X POST \
+-F 'filename=@test-file.txt' \
+-H 'Content-Type: text/plain' \
+'http://localhost:7071/api/upload?filename=test-file.txt&username=jsmith' --verbose
 
-This curl command uses both the querystring and the multi-part form to pass relevant data to and through
-this Azure Function. The querystring property value for `username` becomes the directory name inside the container. 
+This curl command uses 3 pieces of data to successfully use the `parse-multipart` package
+ to pass relevant data to and through this Azure Function:
+* the querystring with filename property
+* the multi-part form (-F) with the filename property
+* the header with the correct content type
+
+The querystring property value for `username` becomes the directory name inside the container. 
 The querystring property value for `filename` becomes the file name used in the container. Both these 
 querystring values are used in the `function.json` to construct the container location: `"path": "images/{username}/{filename}",`.
+
+Before using deployed Azure function
+1. Make sure you append the Function key as the `code` property to the end of the url in your curl command: 
+    curl -X POST \
+    -F 'filename=@test-file.txt' \
+    -H 'Content-Type: text/plain' \
+    'http://localhost:7071/api/upload?filename=test-file.txt&username=jsmith&code=123' --verbose
+2. Set the Azure Storage connection string for the Function in the `AzureWebJobsStorage` property
 
 Getting errors?
 If you don't pass the `filename` in the form or send the `content-type` in the header, 
@@ -62,6 +78,8 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
             context.res.body = `File buffer is incorrect`;
             context.res.status = HTTP_CODES.BAD_REQUEST
         }
+
+        // filename is a required property of the parse-multipart package
         if(parts[0]?.filename)console.log(`Original filename = ${parts[0]?.filename}`);
         if(parts[0]?.type)console.log(`Content type = ${parts[0]?.type}`);
         if(parts[0]?.data?.length)console.log(`Size = ${parts[0]?.data?.length}`);
