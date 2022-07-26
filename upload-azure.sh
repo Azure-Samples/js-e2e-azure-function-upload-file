303a9759f50b924cc4760a1b0880a321f46e6d9d
Dina Berry <41597107+diberry@users.noreply.github.com>
2022-07-26 12:39:01 -0700

    Update upload-azure.sh

--- upload-azure.sh
+++ upload-azure.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-FUNCTION_APP_RESOURCE_NAME="blob-storage-upload-function-app-jsmith"
-FUNCTION_KEY="123456"
-
-FUNCTION_URL="https://${FUNCTION_APP_RESOURCE_NAME}.azurewebsites.net/api/upload?code=${FUNCTION_KEY}&filename=test-file.txt&username=jsmith"
+FUNCTION_URL="https://YOUR-RESOURCE-NAME.azurewebsites.net/api/upload?code=YOUR-FUNCTION-KEY"
 
 echo "${FUNCTION_URL}"
 
 curl -X POST \
 -F "filename=@test-file.txt" \
 -H "Content-Type: text/plain" \
-$FUNCTION_URL --verbose
+"$FUNCTION_URL&filename=test-file.txt&username=jsmith" --verbose
